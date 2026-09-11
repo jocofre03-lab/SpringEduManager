7005571609c57caa692c98e2a5fc59b7b84dbd07
Josue <jo.cofrethomas03@icloud.com>
2026-09-11 13:25:09 -0300

    Add JPA and H2 setup, Course entity and CourseRepository

---
+++ src/main/java/cl/untec/springedumanager/model/Course.java
@@ -0,0 +1,43 @@
+package cl.untec.springedumanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
